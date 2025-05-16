48ccfe9381b342a0f5e566f52ba5ed9f95bae7d2
unknown <michaelmoraes718@gmail.com>
2025-05-16 10:31:57 -0300

    feat: Atividade 2 adicionada

--- obj/Debug/net9.0/c#.AssemblyInfo.cs
+++ obj/Debug/net9.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0177af0c9cbadfc6ea1d453171b90f3222d4531d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99212382ebcbd0228a88d3689d51ff028e055e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
