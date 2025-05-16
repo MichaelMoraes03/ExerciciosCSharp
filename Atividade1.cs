0177af0c9cbadfc6ea1d453171b90f3222d4531d
unknown <michaelmoraes718@gmail.com>
2025-05-16 10:08:59 -0300

    feat: primeiro commit Exercício 1

---
+++ Atividade1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int CapTotal = 10;
+        int PartCad = 9;
+        bool resultado;
+
+        Console.WriteLine("Você deseja visualizar a capacidade total de ingressos? (y/n): ");
+        string escolha = Console.ReadLine().ToLower();
+        if (escolha == "y")
+        {
+            resultado = true;
+            Console.WriteLine("A capacidade total é de " + CapTotal);
+        }
+
+        else if (escolha == "n")
+        {
+            resultado = false;
+            Console.WriteLine("OK");
+        }
+
+        Console.WriteLine("Você deseja ver o número de participantes cadastrados? (y/n): ");
+        escolha = Console.ReadLine().ToLower();
+        if (escolha == "y")
+        {
+            Console.WriteLine("O número de participantes cadastrados é: " + PartCad);
+
+        }
+        else if (escolha == "n")
+        {
+            Console.WriteLine("OK");
+        }
+
+
+        Console.WriteLine("Deseja verificar se pode cadastrar novos usuários e cadastrar novos usuários? (y/n):");
+        escolha = Console.ReadLine().ToLower();
+
+        if (escolha == "y")
+        {
+            if (CapTotal > PartCad)
+            {
+                Console.WriteLine("Pode cadastrar mais");
+            }
+
+        }
+        else if (escolha == "n")
+        {
+            Console.WriteLine("Usurário não pode ser cadastrado");
+        }
+    }
+}
